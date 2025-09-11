f92e158d86a32540c047ef9acf442034139de1be
YNazymko12 <yulychka12@gmail.com>
2025-09-11 16:50:05 +0200

    added helm-chart

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "eks" {
   source          = "./modules/eks"          
   cluster_name    = "eks-cluster-demo"            # Назва кластера
   subnet_ids      = module.vpc.public_subnets     # ID підмереж
-  instance_type   = "t2.micro"                    # Тип інстансів
+  instance_type   = "t3.medium"                    # Тип інстансів
   desired_size    = 1                             # Бажана кількість нодів
   max_size        = 2                             # Максимальна кількість нодів
   min_size        = 1                             # Мінімальна кількість нодів
